b23069dc0021bccd81f0de40b7bc48ffb7b9d537
Spencer Hahmo <schahmo@gmail.com>
2023-03-01 19:23:03 -0700

    Implement createTweetElement function in client.js in addition to cleaning up the code in index.html and tweets.css

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,66 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(() => {
+  
+  // Fake data taken from initial-tweets.json
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+  
+  const renderTweets = (tweets) => {
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      console.log($tweet);
+      $('#tweets-container').append($tweet);
+    }
+  };
+  
+  const createTweetElement = (tweet) => {
+    const $tweet = $(`
+    <article class="tweet">
+      <header class="tweet-header">
+        <div class="user">
+          <img class="user-icon" src="${tweet.user.avatars}">
+          <h3 class="user-name">${tweet.user.name}</h3>
+        </div>
+        <div>
+          <h3 class="user-handle">${tweet.user.handle}</h3>
+        </div>
+      </header>
+      <div class="tweet-text">${tweet.content.text}</div>
+      <footer class="tweet-footer">
+        <span class="tweet-date">${timeago.format(tweet.created_at)}</span>
+        <div class="tweet-responses">
+          <i class="fa-sharp fa-solid fa-flag"></i>
+          <i class="fa-sharp fa-solid fa-retweet"></i>
+          <i class="fa-sharp fa-solid fa-heart"></i>
+        </div>
+      </footer>
+    </article>`)
+    return $tweet;
+  };
+
+  renderTweets(data);
+});
